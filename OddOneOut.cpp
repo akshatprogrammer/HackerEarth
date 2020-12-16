93a068a60318c7d1a85c703395be81d0d4b1b195
Akshat Jain <67745418+akshatprogrammer@users.noreply.github.com>
2020-12-16 19:15:54 +0530

    Update OddOneOut.cpp

--- OddOneOut.cpp
+++ OddOneOut.cpp
@@ -1,20 +1,3 @@
-/*
-// Sample code to perform I/O:
-
-#include <iostream>
-
-using namespace std;
-
-int main() {
-	int num;
-	cin >> num;										// Reading input from STDIN
-	cout << "Input number is " << num << endl;		// Writing output to STDOUT
-}
-
-// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
-*/
-
-// Write your code here
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
