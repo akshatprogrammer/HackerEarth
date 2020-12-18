cb27632d46c44c254ab529a4279bf56cc4b98790
Akshat Jain <67745418+akshatprogrammer@users.noreply.github.com>
2020-12-18 17:39:05 +0530

    Update Transpose.cpp

--- Transpose.cpp
+++ Transpose.cpp
@@ -12,15 +12,6 @@ int main()
 			cin >> a[i][j];
 		}
 	}
-	/*for(int i=0;i<m;i++)
-	{
-		for(int j=0;j<=i;j++)
-		{
-			int temp = a[i][j];
-			a[i][j] = a[j][i];
-			a[j][i] = temp;
-		}
-	}*/
 	for(int i=0;i<m;i++)
 	{
 		for(int j=0;j<n;j++)
